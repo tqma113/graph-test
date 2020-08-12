3ce3afc38b0766777bd085d4d177116f362c6b19
tqma <tqma@trip.com>
2020-08-12 11:20:19 +0800

    add example

--- parser/fast/example/sample.ts
+++ parser/fast/example/sample.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { createParser } from '../parser'
 
-const input = fs.readFileSync(path.resolve(__dirname, '../../sample.graph') ,'utf-8')
+const input = fs.readFileSync(path.resolve(__dirname, '../../example/sample.graph') ,'utf-8')
 const parser = createParser(input)
 parser.parse()
 console.log(parser)
