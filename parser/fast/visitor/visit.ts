7440cba503d8fea58da7c2bc5a087c75004f7b03
tqma <tqma@trip.com>
2020-08-12 16:36:42 +0800

    add visit

---
+++ parser/fast/visitor/visit.ts
@@ -0,0 +1,85 @@
+import type {
+  Program,
+  InferenceDeclaration,
+  ImportStatement,
+  StartStatement,
+  ExportStatement,
+  Block,
+  Module,
+  StepStatement,
+  IfStatement,
+  SwitchStatement,
+  SwitchBlock,
+  CaseClause,
+  DefaultClause,
+  GotoStatement,
+  ModuleStatement,
+  ModuleItems
+} from '../parser/ast'
+
+export const visit = (program: Program) => {
+
+  const visitProgram = (program: Program) => {
+
+  }
+
+  const visitModuleStatement = (moduleStatement: ModuleStatement) => {
+
+  }
+
+  const visitInferenceDeclaration = (inferenceDeclaration: InferenceDeclaration) => {
+
+  }
+
+  const visitImportStatement = (importStatement: ImportStatement) => {
+
+  }
+
+  const visitModuleItems = (moduleItems: ModuleItems) => {
+
+  }
+
+  const visitModule = (module: Module) => {
+
+  }
+
+  const visitExportStatement = (exportStatement: ExportStatement) => {
+
+  }
+
+  const visitStartStatement = (startStatement: StartStatement) => {
+
+  }
+
+  const visitBlock = (block: Block) => {
+
+  }
+
+  const visitStepStatement = (stepStatement: StepStatement) => {
+
+  }
+
+  const visitIfStatement = (ifStatement: IfStatement) => {
+
+  }
+
+  const visitSwitchStatement = (switchStatement: SwitchStatement) => {
+
+  }
+
+  const visitSwitchBlock = (switchBlock: SwitchBlock) => {
+
+  }
+
+  const visitCaseClause = (caseClause: CaseClause) => {
+
+  }
+
+  const visitDefaultClause = (defaultClause: DefaultClause) => {
+
+  }
+
+  const visitGotoStatement = (gotoStatement: GotoStatement) => {
+
+  }
+}
