29a3b327ebd4dd5061d0d1142ddc468f46feaee3
tqma <tqma@trip.com>
2020-08-24 11:38:39 +0800

    feat(language): format

--- packages/language/src/formatter/index.ts
+++ packages/language/src/formatter/index.ts
@@ -1,26 +1,22 @@
-// @ts-nocheck
-
-import type {
-  Program,
-  ModuleStatement,
-  InferenceDefinition,
-  ImportStatement,
-  ExportStatement,
-  StartStatement,
-  ModuleItems,
-  Module,
-  Block,
-  Statement,
-  StepStatement,
-  IfStatement,
-  SwitchStatement,
-  GotoStatement,
-  SwitchBlock,
-  CaseClause,
-  DefaultClause,
-} from '../parser/ast'
+import { FragmentKind } from '../parser/ast'
+import type { Program } from '../parser/ast'
 
 export const format = (ast: Program): Program => {
-  // TODO:
-  return ast
+  const formatProgram = (program: Program): Program => {
+    const kinds = [
+      FragmentKind.ImportStatement,
+      FragmentKind.ExportStatement,
+      FragmentKind.StartStatement,
+      FragmentKind.InferenceDefinition,
+    ] as const
+    program.moduleStatemens = program.moduleStatemens.sort((a, b) => {
+      const index = kinds.indexOf(a.kind)
+      const ks = kinds.slice(0, index + 1)
+      const needToggle = ks.includes(b.kind)
+      return needToggle ? 1 : -1
+    })
+    return program
+  }
+
+  return formatProgram(ast)
 }
